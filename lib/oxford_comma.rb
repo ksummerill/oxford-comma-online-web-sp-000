52f3aadf8208123e33f990ea991b6e9e3b52577c
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-21 15:30:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
   return nil if array.nil?
   return array.join if array.length == 1
   return array.join(" and ") if array.length == 2
-  return array[0..-1].join(', ') + " and " if array.length > 2
+  return array[0..-2].join(', ') + " and " if array.length > 2
 end
 
 # def oxford_comma(array)
