aa04317f6f13409ee14ece804cc238186fb7bb0f
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-21 14:55:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
+# returns a string without any additional fomatting when given a 1-element array
+# adds 'and' between elements when given a 2-element array
+# adds commas plus a final 'and' when given a 3-element array
+# correctly formats arrays of lengths greater than three
+
 def oxford_comma(array)
 
 end
