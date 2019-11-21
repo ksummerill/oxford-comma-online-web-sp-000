d0f4c4af8614087953f434c14aa2c92d7a86fd79
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-21 15:08:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
     if array.length == 1
       return new_array
     elsif array.length == 2
-      return new_array.join(" and ")
+      return [new_array].join(" and ")
     end
   end
 end
