a9777e7d4c288d565d0b6fd81b975ba442aa14d3
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-21 15:47:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,15 +3,6 @@
 # adds commas plus a final 'and' when given a 3-element array
 # correctly formats arrays of lengths greater than three
 
-# def oxford_comma(array)
-#     if array.length == 1
-#       return array.join
-#     elsif array.length == 2
-#       return array.join(" and ")
-#     else array.length >= 3
-#       return array.to_sentence
-#     end
-# end
 
 def oxford_comma(array)
   return nil if array.nil?
@@ -20,10 +11,3 @@ def oxford_comma(array)
   return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 2
 end
 
-
-# arrays
-# ["kiwi"]
-# ["kiwi", "durian"]
-# ["kiwi", "durian", "starfruit"]
-# ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
-# ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
