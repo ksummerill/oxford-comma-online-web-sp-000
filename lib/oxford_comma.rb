f41a7a5f174213e384db60557e21607950d68df5
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-21 15:27:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@
 
 def oxford_comma(array)
   return nil if array.nil?
-    return array[0] if array.length == 1
-    return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
+  return array.join if array.length == 1
+  return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
 end
 
 # def oxford_comma(array)
